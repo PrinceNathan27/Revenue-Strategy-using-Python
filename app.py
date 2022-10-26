522a56eb225c3193e6bb75da1d3def22ab0da19f
PRINCE NATHAN S <84620411+PrinceNathan27@users.noreply.github.com>
2022-10-26 06:03:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-
 df = pd.read_csv('SampleSuperstore.csv')  
-df['Postal Code'] = df['Postal Code'].astype('object')
-df.drop_duplicates(subset=None,keep='first',inplace=True)
-corr = df.corr()
-fig2 = plt.figure(figsize=(10, 4))
-sns.heatmap(corr,annot=True,cmap='Reds')
-st.pyplot(fig2)
+
 
 #plot1
 fig1=plt.figure(figsize=(15,12))
@@ -22,11 +16,10 @@ sns.countplot(x='State',data=df,palette='rocket_r',order=df['State'].value_count
 plt.xticks(rotation=90)
 st.pyplot(fig1)
 
+
 #various plot-2
 ps = df.groupby('State')[['Sales','Profit']].sum().sort_values(by='Sales',ascending=False)
-
 state = st.selectbox("Select State: ", list(ps.index))
-
 fig, ax = plt.subplots(figsize=(2,3))
 ax.set_title(state.upper()+ " Sales and Profit")
 
@@ -40,3 +33,18 @@ ax.bar(ind, ps.loc[state]['Sales'], width, color='royalblue')
 ax.bar(ind+width, ps.loc[state]['Profit'], width, color='orange')
 
 st.pyplot(fig)
+
+#plot-3
+df['Postal Code'] = df['Postal Code'].astype('object')
+df.drop_duplicates(subset=None,keep='first',inplace=True)
+corr = df.corr()
+fig2 = plt.figure(figsize=(10, 4))
+sns.heatmap(corr,annot=True,cmap='Reds')
+st.pyplot(fig2)
+
+#plot4
+df['Region'].value_counts().plot.pie(autopct = '%1.1f%%')
+fig4=plt.figure(figsize=(10,8))
+st.pyplot(fig4)
+
+
